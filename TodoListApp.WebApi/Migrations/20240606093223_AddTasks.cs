d4ba4198ec713d2641d578f6342b2d701a2dfefe
m.pitel <matveypitel06@gmail.com>
2024-06-06 14:48:28 +0500

    Add migrations files

---
+++ TodoListApp.WebApi/Migrations/20240606093223_AddTasks.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoListApp.WebApi.Migrations
+{
+    public partial class AddTasks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoList", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Assignee = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TodoListId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskItem_TodoList_TodoListId",
+                        column: x => x.TodoListId,
+                        principalTable: "TodoList",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskItemEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Assignee = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TodoListId = table.Column<int>(type: "int", nullable: false),
+                    TodoListEntityId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItemEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskItemEntity_TodoList_TodoListId",
+                        column: x => x.TodoListId,
+                        principalTable: "TodoList",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskItemEntity_TodoLists_TodoListEntityId",
+                        column: x => x.TodoListEntityId,
+                        principalTable: "TodoLists",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskItem_TodoListId",
+                table: "TaskItem",
+                column: "TodoListId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskItemEntity_TodoListEntityId",
+                table: "TaskItemEntity",
+                column: "TodoListEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskItemEntity_TodoListId",
+                table: "TaskItemEntity",
+                column: "TodoListId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskItem");
+
+            migrationBuilder.DropTable(
+                name: "TaskItemEntity");
+
+            migrationBuilder.DropTable(
+                name: "TodoList");
+        }
+    }
+}
