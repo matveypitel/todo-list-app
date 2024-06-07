580bf16402597f7fa71080a3d0ad5f6cb7f55461
m.pitel <matveypitel06@gmail.com>
2024-06-07 15:34:04 +0500

    Change the paginated model

--- TodoListApp.Models/DTOs/PagedResult.cs
+++ TodoListApp.Models/DTOs/PagedResult.cs
@@ -5,4 +5,10 @@ public class PagedResult<T>
     public IEnumerable<T> Items { get; set; } = new List<T>();
 
     public int TotalCount { get; set; }
+
+    public int ItemsPerPage { get; set; }
+
+    public int CurrentPage { get; set; }
+
+    public int TotalPages => (int)Math.Ceiling((decimal)this.TotalCount / this.ItemsPerPage);
 }
