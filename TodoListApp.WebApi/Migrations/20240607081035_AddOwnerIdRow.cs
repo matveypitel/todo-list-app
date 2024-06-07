591729f6baa920011ce898454c0a0d19f0cb8c41
m.pitel <matveypitel06@gmail.com>
2024-06-07 15:40:44 +0500

    Add new migrations to tasks

---
+++ TodoListApp.WebApi/Migrations/20240607081035_AddOwnerIdRow.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoListApp.WebApi.Migrations
+{
+    public partial class AddOwnerIdRow : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Tasks");
+        }
+    }
+}
