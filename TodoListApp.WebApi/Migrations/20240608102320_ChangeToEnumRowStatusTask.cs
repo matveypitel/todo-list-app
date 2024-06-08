fb83722e4609eb65201a00755a7de64b97987456
m.pitel <matveypitel06@gmail.com>
2024-06-08 15:52:28 +0500

    new migrations 'task status from string to enum'

---
+++ TodoListApp.WebApi/Migrations/20240608102320_ChangeToEnumRowStatusTask.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoListApp.WebApi.Migrations
+{
+    public partial class ChangeToEnumRowStatusTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Tasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOverDue",
+                table: "Tasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "IsOverDue",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
