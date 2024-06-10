f163d3911adef7f1fdaeee20a997688977cd99df
m.pitel <matveypitel06@gmail.com>
2024-06-10 17:10:22 +0500

    Rename from 'Paginated' to 'Paged' ApiMappingProfile

---
+++ TodoListApp.WebApi/MappingProfiles/PagedApiMappingProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using TodoListApp.Models.Domains;
+using TodoListApp.Models.DTOs;
+using TodoListApp.WebApi.Data.Entities;
+
+namespace TodoListApp.WebApi.MappingProfiles;
+
+public class PagedApiMappingProfile : Profile
+{
+    public PagedApiMappingProfile()
+    {
+        _ = this.CreateMap<PagedModel<TodoListEntity>, PagedModel<TodoList>>().ReverseMap();
+        _ = this.CreateMap<PagedModel<TodoListModel>, PagedModel<TodoList>>().ReverseMap();
+
+        _ = this.CreateMap<PagedModel<TaskItemEntity>, PagedModel<TaskItem>>().ReverseMap();
+        _ = this.CreateMap<PagedModel<TaskItemModel>, PagedModel<TaskItem>>().ReverseMap();
+    }
+}
